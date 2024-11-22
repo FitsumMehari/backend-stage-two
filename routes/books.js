dbab219c3dcf4fce3c4e23537ac8bd11c0791392
FitsumMehari <m.fitsummehari@gmail.com>
2024-11-22 18:36:34 +0300

    fix id to be empty object

--- routes/books.js
+++ routes/books.js
@@ -3,7 +3,7 @@ const Book = require("../models/Book");
 
 router.get("/", async(req, res, next) => {
     try {
-        const books = await Book.find();
+        const books = await Book.find({});
         res.status(200).json(books);
     } catch (error) {
         next(error);
@@ -33,7 +33,7 @@ router.post("/", async(req, res, next) => {
         }
     }
 });
-router.put("/", async(req, res, next) => {
+router.put("/:id", async(req, res, next) => {
     if (!req.body.title ||
         !req.body.author ||
         !req.body.isbn ||
@@ -42,26 +42,29 @@ router.put("/", async(req, res, next) => {
         res.status(400).send("Values are missing!");
     } else {
         try {
-            const book = await Book.findByIdAndUpdate(req.body._id, {
+            const book = await Book.findByIdAndUpdate(req.params.id, {
                 title: req.body.title,
                 author: req.body.author,
                 isbn: req.body.isbn,
                 publishedYear: req.body.publishedYear,
             });
 
-            res.status(201).json({ message: "Book Updated!", book });
+            res.status(201).json({ message: "Book Updated!" });
         } catch (error) {
             next(error);
         }
     }
 });
 router.delete("/:id", async(req, res, next) => {
+    const id = req.params.id
     try {
-        const book = await Book.findById(req.params.id);
+        const book = await Book.findById(id);
         if (!book) {
             res.status(400).json({ message: "Invalid ID" });
         } else {
-            Book.findByIdAndDelete(req.params.id);
+            await Book.findByIdAndDelete(id);
+            res.status(200).json({ message: "Book Deleted" });
+
         }
     } catch (error) {
         next(error);
@@ -69,9 +72,10 @@ router.delete("/:id", async(req, res, next) => {
 });
 router.get("/recommendations", async(req, res, next) => {
     try {
-        const books = await Book.find();
-        const booksLength = books.length() - 1;
-        const randomBookNumber = Math.random() * booksLength;
+        const books = await Book.find({});
+        const booksLength = books.length;
+        const randomBookNumber = Math.floor((Math.random() * booksLength));
+
         const randomBook = await Book.findById(books[randomBookNumber]._id)
         res.status(200).json(randomBook);
     } catch (error) {
