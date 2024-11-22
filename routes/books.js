ebc45b34dacf2f2e38b1ba621efb48e598406db6
FitsumMehari <m.fitsummehari@gmail.com>
2024-11-22 17:29:22 +0300

    intialize books route

---
+++ routes/books.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+
+router.get("/", async(req, res, next) => {});
+router.post("/", async(req, res, next) => {});
+router.put("/", async(req, res, next) => {});
+router.delete("/", async(req, res, next) => {});
+router.get("/recommendations", async(req, res, next) => {});
+
+module.exports = router;
